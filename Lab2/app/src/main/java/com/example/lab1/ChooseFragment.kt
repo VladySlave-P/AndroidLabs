b63b5851839859eabf34c386eca3544f30060e9b
VladySlave-P <vlad.polishuk.007@gmail.com>
2021-12-09 08:56:14 +0200

    Lab2 finished v2.0

---
+++ Lab2/app/src/main/java/com/example/lab1/ChooseFragment.kt
@@ -0,0 +1,174 @@
+package com.example.lab1
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import com.example.lab1.R
+import java.lang.ClassCastException
+
+class ChooseFragment : Fragment() {
+
+    private lateinit var onTextSentListener: OnTextSent
+
+    interface OnTextSent {
+        fun sendData(
+            resName: String,
+            resSize: String,
+            resDough: String
+        )
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        try {
+            onTextSentListener = context as OnTextSent
+        } catch (e: ClassCastException) {
+            throw ClassCastException(
+                activity.toString()
+                        + " must implement onTextSent"
+            )
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.choose_fragment, container, false)
+    }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val pizzaName = view?.findViewById(R.id.PizzasName) as TextView
+
+        val bigSize = view?.findViewById(R.id.BigSize) as CheckBox
+        val middleSize = view?.findViewById(R.id.MiddleSize) as CheckBox
+        val smallSize = view?.findViewById(R.id.SmallSize) as CheckBox
+
+        val standardDough = view?.findViewById(R.id.StandardDough) as CheckBox
+        val thinDough = view?.findViewById(R.id.ThinDough) as CheckBox
+        val philadelphiaDough = view?.findViewById(R.id.PhiladelphiaDough) as CheckBox
+        val boardHotDog = view?.findViewById(R.id.BoardHotDog) as CheckBox
+
+        val okButton = view?.findViewById(R.id.okButton) as Button
+        val clearButton = view?.findViewById(R.id.clearButton) as Button
+
+        var resName = pizzaName.text.toString()
+
+        var resSize = ""
+        var resDough = ""
+
+
+        bigSize.setOnClickListener {
+            middleSize.isChecked = false
+            smallSize.isChecked = false
+            resSize = bigSize.text.toString()
+        }
+        middleSize.setOnClickListener {
+            bigSize.isChecked = false
+            smallSize.isChecked = false
+            resSize = middleSize.text.toString()
+        }
+        smallSize.setOnClickListener {
+            middleSize.isChecked = false
+            bigSize.isChecked = false
+            resSize = smallSize.text.toString()
+        }
+
+        standardDough.setOnClickListener {
+            thinDough.isChecked = false
+            philadelphiaDough.isChecked = false
+            boardHotDog.isChecked = false
+            resDough = standardDough.text.toString()
+        }
+        thinDough.setOnClickListener {
+            standardDough.isChecked = false
+            philadelphiaDough.isChecked = false
+            boardHotDog.isChecked = false
+            resDough = thinDough.text.toString()
+        }
+        philadelphiaDough.setOnClickListener {
+            thinDough.isChecked = false
+            standardDough.isChecked = false
+            boardHotDog.isChecked = false
+            resDough = philadelphiaDough.text.toString()
+        }
+        boardHotDog.setOnClickListener {
+            thinDough.isChecked = false
+            philadelphiaDough.isChecked = false
+            standardDough.isChecked = false
+            resDough = boardHotDog.text.toString()
+        }
+
+        okButton.setOnClickListener {
+            resName = pizzaName.text.toString()
+
+            if (smallSize.isChecked){
+                resSize= smallSize.text.toString()
+            } else if (middleSize.isChecked){
+                resSize= middleSize.text.toString()
+            } else if (bigSize.isChecked) {
+                resSize= bigSize.text.toString()
+            } else {
+                resSize= ""
+            }
+
+            if (thinDough.isChecked){
+                resDough= thinDough.text.toString()
+            } else if (philadelphiaDough.isChecked){
+                resDough= philadelphiaDough.text.toString()
+            } else if (standardDough.isChecked) {
+                resDough= standardDough.text.toString()
+            } else if (boardHotDog.isChecked) {
+                resDough= boardHotDog.text.toString()
+            } else {
+                resDough= ""
+            }
+
+            if(resName != "" && resSize!="" && resDough!="" && resName != "Заповніть всі поля" && resName != "Введіть назву піци") {
+                passData(
+                    resName,
+                    resSize,
+                    resDough
+                )
+            } else {
+                pizzaName.text = "Заповніть всі поля";
+            }
+        }
+
+        clearButton.setOnClickListener {
+            thinDough.isChecked = false
+            philadelphiaDough.isChecked = false
+            standardDough.isChecked = false
+            boardHotDog.isChecked = false
+
+            bigSize.isChecked = false
+            smallSize.isChecked = false
+            middleSize.isChecked = false
+
+            resName = "Введіть назву піци"
+        }
+
+    }
+
+    private fun passData(
+        resName: String,
+        resSize: String,
+        resDough: String
+    ) {
+        onTextSentListener.sendData(
+            resName,
+            resSize,
+            resDough
+        )
+    }
+
+}
--- Lab2/app/src/main/java/com/example/lab1/MainActivity.kt
+++ Lab2/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -1,100 +1,41 @@
 package com.example.lab1
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.TextView
 import android.widget.Toast
-import androidx.fragment.app.FragmentActivity
 
-class MainActivity : FragmentActivity() {
+class MainActivity  : AppCompatActivity(), ChooseFragment.OnTextSent {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val pizzaName = findViewById<TextView>(R.id.PizzasName)
-
-        val chooseSize = findViewById<TextView>(R.id.ChooseSize)
-        val bigSize = findViewById<CheckBox>(R.id.BigSize)
-        val middleSize = findViewById<CheckBox>(R.id.MiddleSize)
-        val smallSize = findViewById<CheckBox>(R.id.SmallSize)
-
-        val chooseDough = findViewById<TextView>(R.id.ChooseDough)
-        val standardDough = findViewById<CheckBox>(R.id.StandardDough)
-        val thinDough = findViewById<CheckBox>(R.id.ThinDough)
-        val philadelphiaDough = findViewById<CheckBox>(R.id.PhiladelphiaDough)
-        val boardHotDog = findViewById<CheckBox>(R.id.BoardHotDog)
-
-        val offerText = findViewById<TextView>(R.id.OfferText)
-
-        val okButton = findViewById<Button>(R.id.okButton)
-        val clearButton = findViewById<Button>(R.id.clearButton)
-
-        var resSize = ""
-
-        bigSize.setOnClickListener {
-            middleSize.isChecked = false
-            smallSize.isChecked = false
-            resSize= bigSize.text.toString()
-        }
-        middleSize.setOnClickListener {
-            bigSize.isChecked = false
-            smallSize.isChecked = false
-            resSize= middleSize.text.toString()
-        }
-        smallSize.setOnClickListener {
-            middleSize.isChecked = false
-            bigSize.isChecked = false
-            resSize= smallSize.text.toString()
-        }
-
-        var resDough =""
-
-        standardDough.setOnClickListener {
-            thinDough.isChecked = false
-            philadelphiaDough.isChecked = false
-            boardHotDog.isChecked = false
-            resDough= standardDough.text.toString()
-        }
-        thinDough.setOnClickListener {
-            standardDough.isChecked = false
-            philadelphiaDough.isChecked = false
-            boardHotDog.isChecked = false
-            resDough= thinDough.text.toString()
-        }
-        philadelphiaDough.setOnClickListener {
-            thinDough.isChecked = false
-            standardDough.isChecked = false
-            boardHotDog.isChecked = false
-            resDough= philadelphiaDough.text.toString()
-        }
-        boardHotDog.setOnClickListener {
-            thinDough.isChecked = false
-            philadelphiaDough.isChecked = false
-            standardDough.isChecked = false
-            resDough= boardHotDog.text.toString()
-        }
-
-        okButton.setOnClickListener{
-            if (pizzaName.text.isEmpty() ||
-                (!bigSize.isChecked && !middleSize.isChecked && !smallSize.isChecked) ||
-                (!standardDough.isChecked && !thinDough.isChecked && !philadelphiaDough.isChecked && !boardHotDog.isChecked)) {
-
-                Toast.makeText(baseContext, "Заповніть всі поля", Toast.LENGTH_SHORT).show()
-                }
-            else {
-                offerText.text= offerText.text.toString() +
-                        "\n"+ pizzaName.text.toString()+ " "+ resSize + " "+ resDough
-            }
-        }
-
-        clearButton.setOnClickListener{
-            offerText.text = "Ваше замовлення:"
-        }
-
-
+        val chooseFragment = ChooseFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, chooseFragment)
+            .commit()
     }
+
+    override fun sendData(
+        resName: String,
+        resSize: String,
+        resDough: String
+    ) {
+        val orderFragment = OrderFragment()
+        val bundle = Bundle()
+
+        if (resName.isNotEmpty() && resSize.isNotEmpty() && resDough.isNotEmpty()) {
+            bundle.putString("resName", resName)
+            bundle.putString("resSize", resSize)
+            bundle.putString("resDough", resDough)
+
+            orderFragment.arguments = bundle
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, orderFragment).addToBackStack(null).commit()
+
+        } else {
+            Toast.makeText(applicationContext, "Input field empty!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
